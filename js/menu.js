b3fc2905356de2fe8c420f95456a91b626022439
LuzNerina <nerinalaguna@gmail.com>
2022-08-19 13:48:03 -0300

    ultimos cambios

---
+++ js/menu.js
@@ -0,0 +1,35 @@
+(function(){
+    const openButton = document.querySelector('.nav__menu');
+    const menu = document.querySelector('.nav__link');
+    const closeMenu = document.querySelector('.nav__close');
+
+    openButton.addEventListener('click', ()=>{
+        menu.classList.add('nav__link--show');
+    });
+
+    closeMenu.addEventListener('click', ()=>{
+        menu.classList.remove('nav__link--show');
+    });
+
+    
+
+
+})();
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
